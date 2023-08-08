a526d423ab42c3d84cf722094897c74da7e96856
Shane Scanlon <shane@turtlestack.dev>
2023-08-08 17:58:45 -0400

    add keygen to install script

--- bin/install.sh
+++ bin/install.sh
@@ -7,3 +7,10 @@ if [ $? -ne 0 ]; then
 fi
 sudo python3 -m pip install ansible
 ansible-playbook playbooks/main.yml
+
+ssh-keygen -t ed25519
+
+echo "The following is your public ssh key. Copy it and add it to your GitHub profile."
+echo "learn more here: "
+echo "https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
+cat ~/.ssh/id_ed25519.pub
