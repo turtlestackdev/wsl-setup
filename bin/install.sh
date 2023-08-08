c944f63ed44d29a5984cb869ecb8a2648e9becb2
Shane Scanlon <shane@turtlestack.dev>
2023-08-08 12:28:33 -0400

    explicitly use sudo for pip install

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,8 @@
 
 python3 -m pip -V
 if [ $? -ne 0 ]; then
-    sudo apt-get update && apt-get install python3-pip
+    sudo apt-get update
+    sudo apt-get install python3-pip
 fi
 python3 -m pip install --user ansible
 ansible-playbook playbooks/main.yml --check
