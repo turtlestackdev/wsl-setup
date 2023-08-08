7b90015c2ec223abab94fe1c1eed9bbbd8fc3c8c
Shane Scanlon <shane@turtlestack.dev>
2023-08-08 12:39:31 -0400

    remove --user from ansible install

--- bin/install.sh
+++ bin/install.sh
@@ -5,5 +5,5 @@ if [ $? -ne 0 ]; then
     sudo apt-get update
     sudo apt-get install python3-pip
 fi
-sudo python3 -m pip install --user ansible
+sudo python3 -m pip install ansible
 ansible-playbook playbooks/main.yml --check
