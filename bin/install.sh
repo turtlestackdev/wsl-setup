add8cd5898d44ad55146159ed2b479ecd78a603e
Shane Scanlon <shane@turtlestack.dev>
2023-08-08 12:25:34 -0400

    check if pip exists, run playbook in check mode

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
+
+python3 -m pip -V
+if [ $? -ne 0 ]; then
+    sudo apt-get update && apt-get install pip3
+fi
 python3 -m pip install --user ansible
-ansible-playbook playbooks/main.yml
+ansible-playbook playbooks/main.yml --check
